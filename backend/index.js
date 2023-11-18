0c9bf9d18c5273bbc631fc065e5cd7027db948a2
ShivamTaneja <shiivamtaneja@gmail.com>
2023-11-18 15:26:01 +0530

    add user data fetch end point
    Signed-off-by: ShivamTaneja <shiivamtaneja@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 
@@ -10,6 +11,11 @@ const corsOptions = {
   credentials: true, // Allow cookies and authentication headers to be sent
 };
 
+
+const prisma = new PrismaClient();
+
+require('dotenv').config();
+
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
@@ -17,5 +23,29 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/getdata', async (req, res) => {
+  try {
+
+    const user = await prisma.user.findMany();
+
+    if (!user) {
+      return res.send({
+        statusCode: 200,
+        message: { msg: "User Not found" }
+      });
+    }
+
+    res.send({
+      statusCode: 200,
+      user
+    });
+
+  } catch (err) {
+    console.error('Error fetching user data:', err);
+  } finally {
+    await prisma.$disconnect();
+  }
+});
 
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
